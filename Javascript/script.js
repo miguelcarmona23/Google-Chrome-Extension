db7db4c8f4b2701b196c72c46fab0e2e542bc9b1
miguelcarmona23 <miguelcarmona996@gmail.com>
2019-12-11 16:06:13 -0500

    Updated Time function

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,61 +1,61 @@
-$(document).ready(function($){
-    //current time
-    //update every 10 seconds;
+$(document).ready(function(){
+  //current time
+  //update every 10 seconds;
+  setCurrentTime();
+  setInterval(function(){
     setCurrentTime();
-    setInterval(function(){
-      setCurrentTime();
-    },10*1000);
-  
-    var username = getCookie('username');
-    //check cookie
-    if(username){
-      $('.greeting').css('display','inline-block');
-      $('.user-name').css('display','none');
-      $('.greeting').html(`Hello <span class="stored-name">${username}</span>.`);
-    }else{
-      $('.greeting').css('display','none');
-      $('.user-name').css('display','inline-block');
-      $('.greeting').html(`What's your name?`);
-    }
-  
-    $('.user-name').keypress(function(e) {
-      if(e.which == 13) {
-        var username = e.target.value;
-        if(!username) return;
-        $('.user-name').fadeOut(function(){
-          $('.greeting').html(`Hello ${username}.`);
-          $('.greeting').fadeIn(function(){
-            setCookie('username', username,365);
-          });
+  },10*1000);
+
+  var username = getCookie('username');
+  //check cookie
+  if(username){
+    $('.greeting').css('display','inline-block');
+    $('.user-name').css('display','none');
+    $('.greeting').html(`Hello <span class="stored-name">${username}</span>.`);
+  }else{
+    $('.greeting').css('display','none');
+    $('.user-name').css('display','inline-block');
+    $('.greeting').html(`What's your name?`);
+  }
+
+  $('.user-name').keypress(function(e) {
+    if(e.which == 13) {
+      var username = e.target.value;
+      if(!username) return;
+      $('.user-name').fadeOut(function(){
+        $('.greeting').html(`Hello ${username}.`);
+        $('.greeting').fadeIn(function(){
+          setCookie('username', username,365);
         });
-      }
-    });
-  
+      });
+    }
   });
-  
-  function setCurrentTime(){
-    var now = new Date();
-    $('.time').html(now.getHours()+":"+now.getMinutes())
-    $('.date').html(now.toLocaleDateString('en-US', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' }));
-  }
-  function setCookie(cname,cvalue,exdays) {
-      var d = new Date();
-      d.setTime(d.getTime() + (exdays*24*60*60*1000));
-      var expires = "expires=" + d.toGMTString();
-      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
-  function getCookie(cname) {
-      var name = cname + "=";
-      var decodedCookie = decodeURIComponent(document.cookie);
-      var ca = decodedCookie.split(';');
-      for(var i = 0; i < ca.length; i++) {
-          var c = ca[i];
-          while (c.charAt(0) == ' ') {
-              c = c.substring(1);
-          }
-          if (c.indexOf(name) == 0) {
-              return c.substring(name.length, c.length);
-          }
-      }
-      return "";
-  }
+
+});
+
+function setCurrentTime(){
+  var now = new Date();
+  $('.time').html(now.getHours()+":"+now.getMinutes())
+  $('.date').html(now.toLocaleDateString('en-US', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' }));
+}
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
